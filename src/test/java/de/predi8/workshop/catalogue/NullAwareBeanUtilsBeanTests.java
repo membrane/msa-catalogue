9c02e2ab2dce8fc213252337c7e47f0c7e7d9687
oliver <weiler@predic8.de>
2017-12-20 14:45:09 +0100

    Add NullAwareBeanUtilsBeanTests

---
+++ src/test/java/de/predi8/workshop/catalogue/NullAwareBeanUtilsBeanTests.java
@@ -0,0 +1,22 @@
+package de.predi8.workshop.catalogue;
+
+import de.predi8.workshop.catalogue.domain.Article;
+import de.predi8.workshop.catalogue.event.NullAwareBeanUtilsBean;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NullAwareBeanUtilsBeanTests {
+	@Test
+	public void testNullAwareBean() throws InvocationTargetException, IllegalAccessException {
+		Article orig = new Article("123", "abc", new BigDecimal(1.99));
+		Article dst = new Article("123", null, null);
+
+		new NullAwareBeanUtilsBean().copyProperties(dst, orig);
+
+		assertThat(dst).hasFieldOrPropertyWithValue("name", "abc");
+	}
+}
