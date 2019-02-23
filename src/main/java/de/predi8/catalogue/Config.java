cdea4f70b3f33824827f5771594a5271892c11e8
Thomas Bayer <bayer@predic8.de>
2019-02-23 18:13:25 +0100

    Added config class

---
+++ src/main/java/de/predi8/catalogue/Config.java
@@ -0,0 +1,23 @@
+package com.predic8.stock;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+
+@Configuration
+public class Config {
+
+    @Bean
+    ObjectMapper mapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.addMixIn(Object.class, IgnoreConfig.class);
+        mapper.enable( INDENT_OUTPUT);
+        return mapper;
+    }
+
+    @JsonIgnoreProperties(value = {"hibernateLazyInitializer"}, ignoreUnknown = true)
+    private abstract class IgnoreConfig { }
+}
--- src/main/java/de/predi8/catalogue/model/Article.java
+++ src/main/java/de/predi8/catalogue/model/Article.java
@@ -5,7 +5,6 @@ import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
-@JsonIgnoreProperties( value =  {"hibernateLazyInitializer", "handler"},ignoreUnknown = true)
 public class Article {
 
 	@Id
