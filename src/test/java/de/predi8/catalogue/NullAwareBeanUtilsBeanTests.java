abe6f2fadc3ff8ed7043cb91ef7248c4e358f8a7
Thomas Bayer <bayer@predic8.de>
2019-11-29 11:52:41 +0100

    Update to 2.1.10

--- src/test/java/de/predi8/catalogue/NullAwareBeanUtilsBeanTests.java
+++ src/test/java/de/predi8/catalogue/NullAwareBeanUtilsBeanTests.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NullAwareBeanUtilsBeanTests {
+
 	@Test
 	public void testNullAwareBean() throws InvocationTargetException, IllegalAccessException {
 		Article orig = new Article("123", "abc", new BigDecimal(1.99));
